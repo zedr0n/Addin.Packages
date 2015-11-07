af7e4fb68f3b42b3880808be3f155e5959c6152a
zedr0n <zedr0nre@gmail.com>
2015-11-07 01:22:46 +0000

    add source code injection

---
+++ CommonAddin/content/MyAddin.cs.pp
@@ -0,0 +1,18 @@
+﻿using CommonAddin;
+using ExcelDna.Integration;
+
+namespace $rootnamespace$
+{
+    public class Test
+    {
+        [ExcelFunction]
+        public static string TestExcel()
+        {
+            return "Test completed";
+        }
+    }
+
+    public class $rootnamespace$ : ExcelAddin
+    {
+    }
+}
--- ExcelInterfaces/Properties/AssemblyInfo.cs
+++ ExcelInterfaces/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ExcelInterfaces")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Interface for CommonAddin")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Zedr0n")]
 [assembly: AssemblyProduct("ExcelInterfaces")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
