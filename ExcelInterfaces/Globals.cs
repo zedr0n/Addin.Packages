e66fce4bac911ba6a33edfd23044483bb05f9ae5
zedr0n <zedr0nre@gmail.com>
2015-11-28 20:18:11 +0000

    add public object factory method

--- ExcelInterfaces/Globals.cs
+++ ExcelInterfaces/Globals.cs
@@ -7,7 +7,7 @@ using System.Xml.Serialization;
 
 namespace ExcelInterfaces
 {
-    public interface IPublicObject
+    public interface IPublicObject : ICloneable
     {
         string Handle { get; set; }
     }
@@ -31,15 +31,15 @@ namespace ExcelInterfaces
             return publicObject;
         }
 
-        public static string WriteToXml2(this IPublicObject obj) 
+        public static string WriteToXml2(this IPublicObject obj)
         {
             var fieldList = new List<FieldInfo>(obj.GetType().GetFields()
                 .Where(info => info.IsPublic))
-                .Where(info => info.GetValue(obj).GetType().GetInterfaces().Contains(typeof(IPublicObject)))
+                .Where(info => info.GetValue(obj).GetType().GetInterfaces().Contains(typeof (IPublicObject)))
                 .Where(info => info.FieldType != info.GetValue(obj).GetType());
             var typesList = fieldList.Select(field => field.GetValue(obj).GetType()).Distinct().ToList();
 
-            var x = new XmlSerializer(obj.GetType(),typesList.ToArray() );
+            var x = new XmlSerializer(obj.GetType(), typesList.ToArray());
             var sw = new StringWriter();
             var ns = new XmlSerializerNamespaces();
             ns.Add("", "");
@@ -48,9 +48,9 @@ namespace ExcelInterfaces
             return sw.ToString();
         }
 
-        public static string WriteToXml<T>(this T obj) where T : IPublicObject, ICloneable
+        public static string WriteToXml<T>(this T obj) where T : IPublicObject
         {
-            var privateObject = obj.Clone();
+            var privateObject = obj.ToPrivate();
 
             var x = new XmlSerializer(privateObject.GetType());
             var sw = new StringWriter();
@@ -64,8 +64,64 @@ namespace ExcelInterfaces
         public static T ReadFromXml<T>(string xml)
         {
             var sr = new StringReader(xml);
-            var x = new XmlSerializer(typeof(T));
-            return (T)x.Deserialize(sr);
+            var x = new XmlSerializer(typeof (T));
+            return (T) x.Deserialize(sr);
+        }
+
+        private static object ToPrivate<T>(this T obj) where T : IPublicObject
+        {
+            return obj.Clone();
+        }
+
+        public static string ToPublic<T>(this T obj, string handle)
+        {
+            var o = obj as IPublicObject;
+            if (o != null)
+                return o.Handle;
+
+            // get all possible derived objects
+            // should be just the corresponding public type
+            var allTypes = AppDomain.CurrentDomain.GetAssemblies()
+                .Where(x => x.GetCustomAttributes(typeof (PublicAttribute), false).Length > 0)
+                .SelectMany(x => x.GetTypes())
+                .Where(x => x.GetInterfaces().Contains(typeof (IPublicObject)));
+            var publicType = allTypes
+                .Where(t => typeof(T).IsAssignableFrom(t))
+                .ToArray().Single();
+
+            // find the (string, baseType) constructor
+            var argumentTypes = new List<Type> {typeof (string), typeof (T)};
+            var constructorInfo = publicType.GetConstructor(argumentTypes.ToArray());
+
+            // and invoke with handle
+            var arguments = new List<object> {handle, obj};
+            var publicObject = (IPublicObject) constructorInfo?.Invoke(arguments.ToArray());
+
+            // enumerate all fields
+            var fieldList = new List<FieldInfo>(typeof (T).GetFields()
+                .Where(info => info.IsPublic));
+            foreach (var fieldInfo in fieldList)
+            {
+                var privateFieldType = fieldInfo.FieldType;
+                // get all public objects derived from the private object field type
+                var publicFieldType = allTypes
+                    .Where(t => t.GetInterfaces().Contains(typeof (IPublicObject)))
+                    .Where(t => privateFieldType.IsAssignableFrom(t))
+                    .ToArray().SingleOrDefault();
+
+                if (publicFieldType == default(Type))
+                    continue;
+
+                // find the (string, baseType) constructor
+                argumentTypes = new List<Type> {typeof (string), privateFieldType};
+                constructorInfo = publicFieldType.GetConstructor(argumentTypes.ToArray());
+                // and invoke with handle_${baseType.Name}
+                arguments = new List<object> {handle + "_" + privateFieldType.Name, fieldInfo.GetValue(publicObject)};
+                // store the constructed public object
+                fieldInfo.SetValue(publicObject, constructorInfo?.Invoke(arguments.ToArray()));
+            }
+
+            return publicObject?.Handle;
         }
     }
 
