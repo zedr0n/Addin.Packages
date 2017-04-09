a70222e37579ca0dd3a2201357486ee0caacc3ea
zedr0n <zedr0nre@gmail.com>
2017-04-09 22:40:49 +0100

    avoid explicit construction of public objects

--- ExcelInterfaces/ICreator.cs
+++ ExcelInterfaces/ICreator.cs
@@ -5,5 +5,7 @@ namespace ExcelInterfaces
     public interface ICreator : IInjectable
     {
         IPublicObject Get(string handle);
+        IPublicObject Create<TPublic, TInstance>(string handle, TInstance instance) where TPublic : Public<TInstance>
+                                                                                    where TInstance : class;
     }
 }
